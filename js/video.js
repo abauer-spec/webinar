742e2fbd865ec4b0e7402985084b39194456a655
abauer-spec <abauer@tradingview-ru.com>
2026-01-16 15:30:08 +0200

    Create video.js

---
+++ js/video.js
@@ -0,0 +1,7 @@
+const options = {
+    roomName: 'TradingView_Live_2026',
+    width: '100%',
+    height: '700px', // Увеличил высоту для удобства торговли
+    parentNode: document.querySelector('#jitsi-container'),
+    // ... остальные настройки
+};
