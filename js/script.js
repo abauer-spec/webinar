472c847e7d59f78cff2806d751d9b390a88d772f
abauer-spec <abauer@tradingview-ru.com>
2026-02-13 11:17:04 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -143,7 +143,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function updateTimer() {
     const now = new Date();
     // ЗАДАТЬ ВРЕМЯ И ДАТУ
-    const targetDate = "2026-02-12T17:00:00+03:00";
+    const targetDate = "2026-02-13T17:00:00+03:00";
     const target = new Date(targetDate);
     const diff = target - now;
 
